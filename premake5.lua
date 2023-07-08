65ce5a1640be7294bbd789faa087f8d2584122d6
almirfilho77 <almir.filho77@gmail.com>
2023-07-08 11:01:28 -0400

    Add premake script to generate project files

---
+++ premake5.lua
@@ -0,0 +1,111 @@
+-- premake5.lua
+
+workspace "CatolYeah"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "CatolYeah"
+	location "CatolYeah"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"CY_PLATFORM_WINDOWS",
+			"CY_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "CY_CONFIG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CY_CONFIG_RELEASE"
+		symbols "Off"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CY_CONFIG_DIST"
+		symbols "Off"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"CatolYeah/vendor/spdlog/include",
+		"CatolYeah/src"
+	}
+
+	links
+	{
+		"CatolYeah"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"CY_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "CY_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CY_RELEASE"
+		symbols "Off"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CY_DIST"
+		symbols "Off"
+		optimize "On"
